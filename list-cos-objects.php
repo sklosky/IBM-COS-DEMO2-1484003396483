5dac13693c2500f90df58cdd9d4b5a0d54cd4151
Stephen Klosky <sklosky@us.ibm.com>
2017-01-09 19:40:33 -0600

    tweaked routine that lists the contents of a specific bucket

--- list-cos-objects.php
+++ list-cos-objects.php
@@ -12,7 +12,7 @@ use Aws\S3\Exception\S3Exception;
 //
 // Receive the bucket name from the get bucket name form
 // 
-$mybucket = $_POST['mybucket'];  // Recieve Subtenant from the main_login form
+$mybucket = $_POST['mybucket'];
 
 //
 // Instantiate the client.
@@ -21,29 +21,27 @@ $s3 = S3Client::factory(array(
     'version' => 'latest',
     'region'  => '',
     'endpoint' => 'http://'.$ENDPOINT,
-//    'profile' => 'pool2',
     'credentials' => array(
-         'key'    => $accesskey,
-         'secret' => $secretkey
+         'key'    => $ACCESSKEY,
+         'secret' => $SECRETKEY
     )
 ));
 
 //
 // Use the plain API (returns ONLY up to 1000 of your objects).
 //
-try {
-    $result = $s3->listObjects(array('Bucket' => $mybucket));
+        try {
+        $result2 = $s3->listObjects(array('Bucket' => $mybucket));
+        } catch (S3Exception $e) {
+        echo "\n";
+        continue ;
+        } 
+        
 
-    echo $mybucket;
-    echo "<br>";
-    foreach ($result['Contents'] as $object) {
-        echo "===> ";
-        echo $object['Key'];
-        echo "<br>";
-    }
-} catch (S3Exception $e) {
-    echo "Bucket not found\n";
-//    echo $e->getMessage() . "\n";
-}
+        foreach ($result2['Contents'] as $object) {
+           echo "<tb>";
+           echo "---> ".$object['Key'] . "\n";
+           echo "<br>";
+           }
 
 ?>
