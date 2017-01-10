925f9fae614c209f37c42006644230c2806ada01
Stephen Klosky <sklosky@us.ibm.com>
2017-01-09 19:57:35 -0600

    adjusting the upload file routine in upload-cos-object.php

--- upload-cos-object.php
+++ upload-cos-object.php
@@ -10,12 +10,15 @@ use Aws\S3\Exception\S3Exception;
 //date_default_timezone_set ( 'America/Los_Angeles' );
 
 //
+// Get the bucket name
+//
+$bucket = $_POST['mybucket'];
+
+//
 // Get the filename from the previous form
 //
-$myuploadfile = $_POST['myfile'];
-$key = $myuploadfile;
-
-$bucket = $_POST['mybucket'];
+$fileName = $_FILES['myfile']['name'];
+$fileTempName = $_FILES['myfile']['tmp_name'];
 
 //
 // Instantiate the client.
@@ -30,11 +33,7 @@ $s3 = S3Client::factory(array(
     )
 ));
 
-//$result =$s3->putObject(array(
-//    'Bucket' => $bucket,
-//    'Key'    => $key,
-//    'SourceFile' => $myuploadfile
-//));
+$result =$s3->putObject($fileTempName, $bucket, $fileName, S3::ACL_PUBLIC_READ);
 
 // Print the .
 print "File uploaded: {$myuploadfile}".PHP_EOL;
