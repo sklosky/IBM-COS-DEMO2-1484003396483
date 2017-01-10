3e3feb60466959284c801c9b1351803457fbde93
Stephen Klosky <sklosky@us.ibm.com>
2017-01-10 14:44:14 -0600

    reprogrammed variables.php to use environmental variables for access keys and endpoints instead of hardcoded values

--- variables.php
+++ variables.php
@@ -1,18 +1,18 @@
 <?php
 
 // these are credentials to Steve's Test Account -- I plan to rotate these regularly
-$ENDPOINT = 's3-api.us-geo.objectstorage.softlayer.net';
-$ACCESSKEY = 'VfAY8vRGl6LvV4XjZaM7';
-$SECRETKEY = 'C6lhiZo1U32iHykYrTCa7SDRP9BUtesLzd7RVdBQ';
+//$ENDPOINT = 's3-api.us-geo.objectstorage.softlayer.net';
+//$ACCESSKEY = 'VfAY8vRGl6LvV4XjZaM7';
+//$SECRETKEY = 'C6lhiZo1U32iHykYrTCa7SDRP9BUtesLzd7RVdBQ';
 
 $myEnvironment = $_ENV;
 if (array_key_exists("ibmCosAccessKey", $myEnvironment)) {
-	echo "ibmCosAccessKey is ".$myEnvironment['ibmCosAccessKey'];
+	$ACCESSKEY = $myEnvironment['ibmCosAccessKey'];
 }
 if (array_key_exists("ibmCosSecretKey", $myEnvironment)) {
-	echo "ibmCosSecretKey is ".$myEnvironment['ibmCosSecretKey'];
+	$SECRETKEY = $myEnvironment['ibmCosSecretKey'];
 }
 if (array_key_exists("ibmCosEndpoint", $myEnvironment)) {
-	echo "ibmCosEndpoint is ".$myEnvironment['ibmCosEndpoint'];
+	$ENDPOINT = $myEnvironment['ibmCosEndpoint'];
 }
 ?>
