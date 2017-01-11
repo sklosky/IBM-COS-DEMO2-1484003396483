28b2d97563805ed15448dcd271037ff39d009cce
Stephen Klosky <sklosky@us.ibm.com>
2017-01-11 19:13:13 +0000

    found out that the environment variables are not sticky, and are lost when the app is redeployed.  Hardcoding for now

--- variables.php
+++ variables.php
@@ -1,9 +1,9 @@
 <?php
 
 // these are credentials to Steve's Test Account -- I plan to rotate these regularly
-//$ENDPOINT = 's3-api.us-geo.objectstorage.softlayer.net';
-//$ACCESSKEY = 'VfAY8vRGl6LvV4XjZaM7';
-//$SECRETKEY = 'C6lhiZo1U32iHykYrTCa7SDRP9BUtesLzd7RVdBQ';
+$ENDPOINT = 's3-api.us-geo.objectstorage.softlayer.net';
+$ACCESSKEY = 'VfAY8vRGl6LvV4XjZaM7';
+$SECRETKEY = 'C6lhiZo1U32iHykYrTCa7SDRP9BUtesLzd7RVdBQ';
 
 //Not sure if the environment variables will survive when the app is redeployed and restarted.
 //Leaving the old settings commented out in the app until we nail down the expected behaviour.
